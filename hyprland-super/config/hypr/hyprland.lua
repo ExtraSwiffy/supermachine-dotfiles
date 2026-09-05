1a7a97836139e829e843f93c87aef98bbc078114
ExtraSwiffy <drake.silvers13@gmail.com>
2026-09-05 04:25:26 -0400

    Increase window gaps and border width

--- hyprland-super/config/hypr/hyprland.lua
+++ hyprland-super/config/hypr/hyprland.lua
@@ -17,8 +17,8 @@ hl.env("HYPRCURSOR_SIZE", "24")
 
 hl.config({
     general = {
-        gaps_in = 8,
-        gaps_out = 8,
+        gaps_in = 12,
+        gaps_out = 12,
         border_size = 2,
         col = {
             active_border = "rgba(86d9d9ee)",
